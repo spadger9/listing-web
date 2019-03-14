2adc908fa331c1d057f81a47fcecc03a62cea69f
spadger9 <shrawinpandey@gmail.com>
2019-03-13 20:14:46 -0500

    NavBar and Listing Board body

---
+++ src/components/ListBoard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class ListBoard extends Component {
+  render() {
+    return (
+      
+      <div className="container">
+      <h2>Nabin, buying this property?</h2>
+      <p>I bet you would like to, but I ain't selling...lol</p>            
+      <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" 
+      className="img-rounded" alt="Cinque Terre" width="304" height="236"></img> 
+    </div>
+      
+
+    );
+  }
+}
+
+export default ListBoard;
